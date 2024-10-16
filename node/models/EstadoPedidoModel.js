4a9cf7442710fab88211fdd5fb5d37055e485dd7
yas <yas.villarroel@duocuc.cl>
2024-10-15 23:20:51 -0300

    test estado pedido

--- node/models/EstadoPedidoModel.js
+++ node/models/EstadoPedidoModel.js
@@ -4,6 +4,9 @@ import { DataTypes } from 'sequelize';
 const EstadoPedidoModel = db.define('estadopedido', {
     ID_Estado: { type: DataTypes.INTEGER, primaryKey: true },
     Estado: { type: DataTypes.STRING }
+},{
+    timestamps: true,
+    tableName: 'pedidoestado'
 });
 
 export default EstadoPedidoModel;
